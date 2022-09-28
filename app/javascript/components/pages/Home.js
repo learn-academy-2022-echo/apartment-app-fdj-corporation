070e493713420403d6cd43fa7b4485072017ecc7
jmagistrado <joycemagistrado@gmail.com>
2022-09-28 16:51:01 -0700

    Working on Home Page. Cannot see Logged in Home Page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -4,44 +4,44 @@ import {Button, NavItem, NavLink} from "reactstrap"
 const Home = (props) => {
   return (
     <>
-    <div className="Body">
-        <h1>Welcome the top Apartment Management Team</h1>
-        <p>Proud serving our customers for over 50 years</p>
-    </div>
-
-    
-    <h3>Home</h3>
     {props.logged_in && (
       <>
-       <h3>Welcome to PDJ Corporation</h3>
+       <h1>Welcome Back!</h1>
+        <p>Please take a look at all our available apartments for lease. As a member of our website, you have the ability to save your listings. See them, head to your "My Listing".</p>
        <NavItem>
         <Button>
-        <NavLink to = {sign_in_route}>
-            Please Sign in
-          </NavLink>
+          {/* Needs to link to the index that is ony belongs to them */}
+          <a href="/apartmentindex">
+              View Your Listings
+            </a>
+          </Button>
+          <Button>
+            <a href="/apartmentindex">
+              All Listings
+            </a>
           </Button>
         </NavItem>
-        </>
+      </>
       )}
       {!props.logged_in && (
-        
-       <NavItem>
-        <Button>
-          <NavLink to = "/apartmentindex" className ="nav-link">
-            Please sign in
-          </NavLink>
-          </Button>
-
-          <Button>
-          <NavLink to = {`/users/sign_up`}>
-            Please Sign up
-          </NavLink>
-          </Button>
-
-        </NavItem>
+        <>
+          <h1>Welcome the top Apartment Management Team</h1>
+            <p>Proud serving our customers for over 50 years. Please take a look at all our available apartments for lease. For a quick and easy way to save your information, create an account!</p>
+              <NavItem>
+                <Button>
+                  <a href="/users/sign_up">
+                    Sign Up
+                  </a>
+                </Button>
+                <Button>
+                  <a href="/users/sign_in">
+                    Sign In
+                  </a>
+                </Button>
+              </NavItem>
+          </>
       )}
   </>
-   
   )
 }
 
