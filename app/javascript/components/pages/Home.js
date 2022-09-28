7906cc4141e91dfb623b95c1ea20a9d3a1cc7b7d
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-28 14:51:48 -0700

    home-page in progress nav-link do not work

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,10 +1,47 @@
 import React from "react"
+import {Button, NavItem, NavLink} from "reactstrap"
 
-const Home = () => {
+const Home = (props) => {
   return (
     <>
-      <h3>Home</h3>
-    </>
+    <div className="Body">
+        <h1>Welcome the top Apartment Management Team</h1>
+        <p>Proud serving our customers for over 50 years</p>
+    </div>
+
+    
+    <h3>Home</h3>
+    {props.logged_in && (
+      <>
+       <h3>Welcome to PDJ Corporation</h3>
+       <NavItem>
+        <Button>
+        <NavLink to = {sign_in_route}>
+            Please Sign in
+          </NavLink>
+          </Button>
+        </NavItem>
+        </>
+      )}
+      {!props.logged_in && (
+        
+       <NavItem>
+        <Button>
+          <NavLink to = "/apartmentindex" className ="nav-link">
+            Please sign in
+          </NavLink>
+          </Button>
+
+          <Button>
+          <NavLink to = {`/users/sign_up`}>
+            Please Sign up
+          </NavLink>
+          </Button>
+
+        </NavItem>
+      )}
+  </>
+   
   )
 }
 
